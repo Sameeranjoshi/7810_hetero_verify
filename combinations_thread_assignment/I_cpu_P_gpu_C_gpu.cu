f98c2926d4e2f3333aa3b90a0dff6d1fe5e95078
Sameeranjoshi <joshisameeran17@gmail.com>
2024-03-19 16:14:24 -0600

    Follow a generic test creation approach, wrote tests for all combinations

--- combinations_thread_assignment/I_cpu_P_gpu_C_gpu.cu
+++ combinations_thread_assignment/I_cpu_P_gpu_C_gpu.cu
@@ -36,7 +36,7 @@ __global__ void init_using_gpu_kernel(atomic<int> *flag, int *data){
 // I needed this weird way of passing pointers to pointers 
 // Need to find a better way, but this is similar to the base code without using 
 // function call, as this maps in the same way as of the base code.
-void allocate_unified_mem(atomic<int> **flag_t, int** data_t, int ** result_t){
+void allocate_unified_mem(atomic<int> **flag_t, int **data_t, int **result_t){
     // Flag in unified memory
     SAFE(cudaMallocManaged(flag_t, sizeof(atomic<int>)));
     SAFE(cudaMallocManaged(data_t, sizeof(int)));
@@ -45,17 +45,36 @@ void allocate_unified_mem(atomic<int> **flag_t, int** data_t, int ** result_t){
 }
 
 void init(atomic<int> *flag, int* data, const char *initis){
-    printf("good");
-    // if (strcmp(initis, "cpu") == 0){
-    //     printf("CPU");
-    //     // Initial values: data = <unknown>, flag = 0
-    //     flag->store(0, memory_order_relaxed);
-    // }
-    // if (strcmp(initis, "gpu")){
-    //     printf("GPU");
-    //     // currently 1 block, 1 thread.
-    //     init_using_gpu_kernel<<<1,1>>>(flag, data);
-    // }
+    if (strcmp(initis, "cpu") == 0){
+        // Initial values: data = <unknown>, flag = 0
+        flag->store(0, memory_order_relaxed);
+    }
+    if (strcmp(initis, "gpu") == 0){
+        // currently 1 block, 1 thread.
+        init_using_gpu_kernel<<<1,1>>>(flag, data);
+    }
+}
+
+void producer(atomic<int> *flag, int* data, int *result, const char *produceris){
+    if (strcmp(produceris, "cpu") == 0){
+        // Producer sequences
+        *data = 42;
+        flag->store(1, memory_order_release);
+    }
+    if (strcmp(produceris, "gpu") == 0){
+        producer_gpu<<<1, 1>>>(flag, data, result);
+    }
+}
+
+void consumer(atomic<int> *flag, int* data, int *result, const char *consumeris){
+    if (strcmp(consumeris, "cpu") == 0){
+        // Consumer sequences
+        while (flag->load(memory_order_acquire) == 0) {}
+        *result = *data;
+    }
+    if (strcmp(consumeris, "gpu") == 0){
+        consumer_gpu<<<1, 1>>>(flag, data, result);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -70,28 +89,29 @@ int main(int argc, char* argv[]) {
     // Result array pinned in CPU memory
     SAFE(cudaMallocHost(&result, sizeof(int)));
 
+    
     // allocate the unified memory 
+    // Error: variable "result" is used before its value is se
+    // using pointer-to-pointer solve above error.
     // allocate_unified_mem(&flag, &data, &result);
 
-    // // choose locations of threads
-    // const char *initis = "cpu";
-    // const char *produceris = "cpu";
-    // const char *consumeris = "cpu";
+    // choose locations of threads
+    const char *initis = "cpu";
+    const char *produceris = "gpu";
+    const char *consumeris = "gpu";
 
-    // init(flag, data, initis);
-    // producer(flag, data, result, produceris);
-    // consumer(flag, data, result, consumeris);
+    // Launch depending on corresponding thread.
+    init(flag, data, initis);
+    producer(flag, data, result, produceris);
+    consumer(flag, data, result, consumeris);
 
-
-    // ////////////////////////////////////////////////////////////////////////////
-    // producer_gpu<<<1,1>>>(flag, data, result);
-    // // Launch the consumer asynchronously
-    // consumer_gpu<<<1,1>>>(flag, data, result);
-
-    // // Wait for consumer to finish
-    // SAFE(cudaDeviceSynchronize());
+    // only synchronize if gpu is involved.
+    if (!(strcmp(initis, "gpu") != 0 && strcmp(produceris, "gpu") != 0 && strcmp(consumeris, "gpu") != 0)) {
+        // // Wait for consumer to finish
+        SAFE(cudaDeviceSynchronize());
+    }
     // // Print the result
-    // printf("%d (expected 42)\n", *result);
+    printf("%d (expected 42)\n", *result);
 
     return 0;
 }
--- combinations_thread_assignment/Readme.md
+++ combinations_thread_assignment/Readme.md
@@ -1,42 +1,40 @@
 Various combinations of tests used in this folder.
 
+Current set of tests : Naming convention I_<init>_P_<producer>_C_<consumer>.cu
+
+
+init: cpu, producer: cpu, consumer: cpu
+init: cpu, producer: cpu, consumer: gpu
+init: cpu, producer: gpu, consumer: cpu
+init: cpu, producer: gpu, consumer: gpu
+
+init: gpu, producer: cpu, consumer: cpu
+init: gpu, producer: cpu, consumer: gpu
+init: gpu, producer: gpu, consumer: cpu
+init: gpu, producer: gpu, consumer: gpu
+
+
+
+In future see if these are interesting to try
+
 Init: CPU, Producer: CPU, Consumer: CPU
-
 Init: CPU, Producer: CPU, Consumer: GPU1
-
 Init: CPU, Producer: CPU, Consumer: GPU2
-
 Init: CPU, Producer: GPU1, Consumer: CPU
-
 Init: CPU, Producer: GPU1, Consumer: GPU1
-
 Init: CPU, Producer: GPU1, Consumer: GPU2
-
 Init: CPU, Producer: GPU2, Consumer: CPU
-
 Init: CPU, Producer: GPU2, Consumer: GPU1
-
 Init: CPU, Producer: GPU2, Consumer: GPU2
-
 Init: GPU1, Producer: CPU, Consumer: CPU
-
 Init: GPU1, Producer: CPU, Consumer: GPU1
-
 Init: GPU1, Producer: CPU, Consumer: GPU2
-
 Init: GPU1, Producer: GPU1, Consumer: CPU
-
 Init: GPU1, Producer: GPU1, Consumer: GPU1
-
 Init: GPU1, Producer: GPU1, Consumer: GPU2
-
 Init: GPU1, Producer: GPU2, Consumer: CPU
-
 Init: GPU1, Producer: GPU2, Consumer: GPU1
-
 Init: GPU1, Producer: GPU2, Consumer: GPU2
-
-
 Init: GPU2, Producer: CPU, Consumer: CPU
 Init: GPU2, Producer: CPU, Consumer: GPU1
 Init: GPU2, Producer: CPU, Consumer: GPU2

f21da7d61428c83f81a018b20187d39f3217d508
Sameeranjoshi <joshisameeran17@gmail.com>
2024-03-25 16:58:50 -0600

    Success to add test which can show different behaviors.(test.cu not base_test...)

--- four_combinations/base_test_daniel_modified.cu
+++ four_combinations/base_test_daniel_modified.cu
@@ -7,57 +7,49 @@
 
 using namespace cuda;
 
-__global__ void consumer(atomic<int>* flag, int* data, int* result) {
-    while (flag->load(memory_order_acquire) == 0) {}
-    *result = *data;
+__global__ void consumer(atomic<int>* flag, int* data, int* result0, int *result1) {
+    *result1 = flag->load(memory_order_acquire);
+    *result0 = *data;
 }
+
 
 #define SAFE(x) if (0 != x) { abort(); }
 
 int main(int argc, char* argv[]) {
     atomic<int>* flag;
     int* data;
-    int* result;
+    int* result0;
+    int* result1;
 
-    int data_in_unified_memory = 1;
+    // int data_in_unified_memory = 1;
 
     ////////////////////////////////////////////////////////////////////////////
+    // Result array pinned in CPU memory
+    SAFE(cudaMallocHost(&result0, sizeof(int)));
+    SAFE(cudaMallocHost(&result1, sizeof(int)));
 
     // Flag in unified memory
     SAFE(cudaMallocManaged(&flag, sizeof(atomic<int>)));
-
     // Data placed as specified
-    if (data_in_unified_memory) {
-        SAFE(cudaMallocManaged(&data, sizeof(int)));
-    } else {
-        SAFE(cudaMalloc(&data, sizeof(int)));
-    }
-
-    // Result array pinned in CPU memory
-    SAFE(cudaMallocHost(&result, sizeof(int)));
-
-    // Initial values: data = <unknown>, flag = 0
+    SAFE(cudaMallocManaged(&data, sizeof(int)));
+    // Initial values: data = 0, flag = 0
     flag->store(0, memory_order_relaxed);
+    data = 0;
 
     ////////////////////////////////////////////////////////////////////////////
 
     // Launch the consumer asynchronously
-    consumer<<<1,1>>>(flag, data, result);
+    consumer<<<1,1>>>(flag, data, result0, result1);
 
     // Producer sequence
-    if (data_in_unified_memory) {
-        *data = 42;
-    } else {
-        int h_data = 42;
-        SAFE(cudaMemcpy(data, &h_data, sizeof(int), cudaMemcpyHostToDevice));
-    }
+    *data = 42;
     flag->store(1, memory_order_release);
 
     // Wait for consumer to finish
     SAFE(cudaDeviceSynchronize());
 
     // Print the result
-    printf("%d (expected 42)\n", *result);
+    printf("R0 = %d, R1 = %d\n", *result0, *result1);
 
     return 0;
 }
